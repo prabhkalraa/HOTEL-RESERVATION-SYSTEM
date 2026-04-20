bef7b3fcc13cb4785e23d708a33586a13d115419
Prabhmeet Singh <prabhkalraa@gmail.com>
2026-04-20 16:53:15 +0530

    Create Room class with properties and constructor

---
+++ Room.java
@@ -0,0 +1,11 @@
+class Room {
+    int roomNumber;
+    String category;
+    boolean isBooked;
+
+    public Room(int roomNumber, String category) {
+        this.roomNumber = roomNumber;
+        this.category = category;
+        this.isBooked = false;
+    }
+}
